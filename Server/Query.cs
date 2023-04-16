6c360260d3dc4fba472e1e44c837a2272d8c9ccf
Viktor Rådberg <thevikeo@gmail.com>
2023-04-16 17:34:09 +0200

    small fixes

--- Server/Query.cs
+++ Server/Query.cs
@@ -13,7 +13,7 @@ namespace JmdictGQL
 
         public IEnumerable<JmdictEntry?> EntriesByReading(string reading)
         {
-            return _jmdict.Entries.Where(e => e.Readings?.FirstOrDefault(r => r.Kana == reading) != null);
+            return _jmdict.Entry.Where(entry => entry.KanjiReading.Any(kr => kr.Reading == reading) != null);
         }
     }
 }
