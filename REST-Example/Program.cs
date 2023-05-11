232964ec80905800906b979f19c5558790896130
Viktor Rådberg <66582202+Vikeo@users.noreply.github.com>
2023-05-11 18:02:19 +0200

    use dependency injection

--- REST-Example/Program.cs
+++ REST-Example/Program.cs
@@ -12,15 +12,14 @@ builder.Services.AddSingleton(jmdict);
 
 var app = builder.Build();
 
-//http://localhost:5207/reading/カラオケ
-app.MapGet("/reading/{reading}", (string reading) =>
-    {
-        //TODO Get entries from the services jmdict instead
-        var entry = jmdict.Entries
+// http://localhost:5207/reading/カラオケ
+app.MapGet("/reading/{reading}", (string reading, Jmdict jmdict) =>
+{
+    var entry = jmdict.Entries
         .Where(e => e.Readings?
-        .FirstOrDefault(r => r.Kana == reading) != null);
+            .FirstOrDefault(r => r.Kana == reading) != null);
 
-        return entry;
-    });
+    return entry;
+});
 
 app.Run();
